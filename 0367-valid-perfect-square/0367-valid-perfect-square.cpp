3b19d871b0c46ce92c238d16b4e501eb3ca08844
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-06-27 12:26:43 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int a = sqrt(num);
+        for(int i=a-2;i<a+2;i++){
+            if(pow(i,2)==num) return true;
+        }
+    return false;
+    }
+};
