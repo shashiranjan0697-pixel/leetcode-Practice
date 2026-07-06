14c6dcf80854ec026fba6a4cfbece254ff072487
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-07-06 13:26:31 +0530

    Time: 0 ms (100%), Space: 15.2 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
-        ans.push_back(arr[0]);
         
-        for(int i=1;i<n;i++){
-            int m = ans.size();
-            vector<int>curr = arr[i];
-            if(curr[1] <= ans[m-1][1] ){
+        sort (arr.begin(), arr.end(), [] (vector<int>&a, vector<int>&b){
+            return   (a[0]==b[0])  ? a[1]>b[1] :  a[0]<b[0];
+        } );
+        int count = 0;
+        int end = arr[0][1];
+        for(int i=1;i<arr.size();i++){
+            if(arr[i][1]<=end){
+                count++;
                 continue;
             }
-            else if (curr[0]<= ans[m-1][0] && curr[1] >= ans[m-1][1]) {
-                ans.pop_back();
-                ans.push_back(arr[i]);
-            }
-            else{
-                ans.push_back(arr[i]);
-            }
+            else end = arr[i][1];
         }
         
-    return ans.size();
+    return arr.size()-count;
     }
 };
