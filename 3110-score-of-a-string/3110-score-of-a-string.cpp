1f994cac1a0e00196fe9634516b1dc6697c0cdb8
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-07-07 11:56:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (31.31%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,8 +3,6 @@ public:
     int scoreOfString(string s) {
         int score = 0;
         for(int i=1; i<s.length(); i++){
-            // int p = s[i-1];
-            // int t = s[i];
             score+=abs(s[i-1] - s[i]);
         }
     return score;
