cacb5882af3410c5816d11589d0c54996b3af35f
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-07-07 11:56:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (91.53%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,9 +3,9 @@ public:
     int scoreOfString(string s) {
         int score = 0;
         for(int i=1; i<s.length(); i++){
-            int p = s[i-1];
-            int t = s[i];
-            score+=abs(t-p);
+            // int p = s[i-1];
+            // int t = s[i];
+            score+=abs(s[i-1] - s[i]);
         }
     return score;
     }
