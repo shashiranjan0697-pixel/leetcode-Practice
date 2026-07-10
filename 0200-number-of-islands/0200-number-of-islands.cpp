a198fc902159cd50e3e46458bf9826774d33876d
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-07-10 15:33:29 +0530

    Time: 27 ms (56.47%), Space: 16.8 MB (58.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -33,6 +33,36 @@ public:
             }
         }
     }
+
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i,int j){
+        int n = grid.size();
+        int m = grid[0].size();
+    
+        visited[i][j]=true;
+
+            // right check
+            if(j<m-1 && !visited[i][j+1] && grid[i][j+1]=='1') {
+                dfs(grid, visited, i, j+1);
+            }
+
+            // left check
+            if(j>0 && !visited[i][j-1] && grid[i][j-1]=='1') {
+                dfs(grid, visited, i, j-1);
+            }
+
+            // top check
+            if(i<n-1 && !visited[i+1][j] && grid[i+1][j]=='1') {
+                dfs(grid, visited, i+1, j);
+            }
+
+            // bottom check
+            if(i>0 && !visited[i-1][j] && grid[i-1][j]=='1') {
+                dfs(grid, visited, i-1, j);
+            }
+        
+    }
+
+
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
@@ -42,7 +72,8 @@ public:
             for(int j=0;j<m;j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
                     visited[i][j] = true;
-                    bfs(grid, visited, i,j);
+                    // bfs(grid, visited, i,j);
+                    dfs(grid, visited, i,j);
                     count++;
                 }
             }
