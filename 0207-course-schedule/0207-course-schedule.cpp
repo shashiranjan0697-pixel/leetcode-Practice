a10edbf36dd14bc9d17c8b48410e4950ce692e14
shashiranjan0697-pixel <shashiranjan0697@gmail.com>
2026-07-11 16:12:54 +0530

    Time: 4 ms (63.79%), Space: 21.1 MB (6.87%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void bfs (vector<int>& ans, vector<bool>& visited, unordered_map<int,vector<int> >& mp, vector<int>& inDeg){
+        queue<int> q;
+
+        for(int i=0;i<inDeg.size();i++){
+            if(inDeg[i]==0){
+                q.push(i);
+                visited[i]=true;
+            }
+        }
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto e : mp[front]){
+                inDeg[e]--;
+                if(inDeg[e]==0 && !visited[e]){
+                    q.push(e);
+                    visited[e] = true;
+                }
+            }
+        }
+    }
+
+    bool canFinish(int n, vector<vector<int>>& arr) {
+        vector<int> ans;
+        vector<int> inDeg(n,0);
+        vector<bool> visited(n, false);
+        unordered_map<int,vector<int> > mp;
+        for(auto ele : arr){
+            int course = ele[0];
+            int prereq = ele[1];
+
+            mp[prereq].push_back(course);
+            inDeg[course]++;
+        }
+         
+        bfs(ans, visited, mp, inDeg);
+        // cout<<ans.size();
+    return n==ans.size();
+    }
+};
