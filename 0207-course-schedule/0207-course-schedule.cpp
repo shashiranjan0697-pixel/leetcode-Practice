0e8cdc2d62b316e3a7f4a10dc19228548dc09490
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-07-12 18:22:21 +0530

    Time: 14 ms (10.51%), Space: 21.3 MB (5.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // kahn's Algorithm
+
     void bfs (vector<int>& ans, unordered_map<int,vector<int> >& mp, vector<int>& inDeg){
         queue<int> q;
 
@@ -20,9 +22,26 @@ public:
             }
         }
     }
+        bool ans = true;
+
+    void dfs(unordered_map<int,vector<int> >& mp, vector<bool>& visited, vector<bool>& path, int i){
+        visited[i] = true;
+        path[i] = true;
+        for(auto ele : mp[i]){
+            if(path[ele]){
+                ans = false;
+                return;
+            }
+            
+            if(!visited[ele])  dfs(mp, visited, path, ele);    
+            
+        }
+        path[i] = false;
+    }
 
     bool canFinish(int n, vector<vector<int>>& arr) {
-        vector<int> ans;
+        // bfs sol
+        // vector<int> ans;
         vector<int> inDeg(n,0);
         unordered_map<int,vector<int> > mp;
         for(auto ele : arr){
@@ -31,9 +50,15 @@ public:
 
             mp[prereq].push_back(course);
             inDeg[course]++;
-        }
-         
-        bfs(ans, mp, inDeg);
-    return n==ans.size();
+        } 
+        // bfs(ans, mp, inDeg);
+    // return n==ans.size();
+
+    // DFS solution 
+        // ans = true;
+        vector<bool> visited(n,false);
+        vector<bool> path(n,false);
+        for(int i=0;i<n;i++ )    dfs(mp, visited, path, i);
+    return ans;
     }
 };
