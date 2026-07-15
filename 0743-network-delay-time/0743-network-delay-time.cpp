29acf21bcba1582b9ea4f2f4098c43fb2afaa963
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-07-15 15:21:42 +0530

    Time: 102 ms (10.17%), Space: 47.9 MB (25.48%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    struct cmp {
+    bool operator()(pair<int,int>& a, pair<int,int>& b) {
+            return a.second > b.second;
+        }
+    };
+
+    void bfs (unordered_map<int, vector<pair<int,int> > >& mp, vector<int>& time, int k){
+        priority_queue<pair<int,int>,
+                    vector<pair<int,int>>, cmp> pq;
+        pq.push({k,0});
+        time[k]=0;
+        while(!pq.empty()){
+            auto top = pq.top();
+            int src = top.first;
+            int wt = top.second;
+            pq.pop();
+
+            
+
+            for(auto ele : mp[src]){
+                int d = ele.first;      // destination
+                int t = ele.second;    // weight
+                if(wt > time[src]) continue;
+                if(time[d] > wt+t){
+                    time[d] = wt+t;
+                    pq.push({d, wt+t});
+                }
+            }
+        }    
+    }
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int,int> > > mp;
+        for(auto ele : times){
+            int src = ele[0], dest = ele[1], wet = ele[2];
+            mp[src].push_back({dest, wet});
+            // mp[dest].push_back({src, wet});
+        }
+
+        vector<int>time(n+1, INT_MAX);
+        time[0]=-1;
+        bfs(mp, time, k);
+        int ans=0;
+        for(auto x : time) ans = max(ans, x);
+        
+    return ans == INT_MAX ? -1 : ans;
+    }
+};
