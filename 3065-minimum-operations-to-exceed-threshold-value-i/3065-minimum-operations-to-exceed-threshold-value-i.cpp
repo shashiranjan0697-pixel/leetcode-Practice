ea8c408f4dc4828fbf94a19cf8984762f7dbcb62
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-07-29 09:58:36 +0530

    Time: 0 ms (100%), Space: 24.9 MB (45.39%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -4,7 +4,6 @@ public:
         sort(nums.begin(),nums.end());
         int ans=0;
         for(auto ele : nums) {
-            cout<<ele<<" ,";
             if(ele>=k) break;
             else ans++;
         }
