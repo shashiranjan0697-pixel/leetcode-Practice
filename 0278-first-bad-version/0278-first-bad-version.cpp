42737a33056a9f4209694184d1715937f3eb7211
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-07-29 09:45:30 +0530

    Time: 0 ms (100%), Space: 8 MB (5.25%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -3,10 +3,8 @@ public:
     int firstBadVersion(int n) {
         int lo = 1;
         int hi = n;
-
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-
             if (isBadVersion(mid)) {
                 hi = mid;
             }
@@ -14,7 +12,6 @@ public:
                 lo = mid + 1;
             }
         }
-
-        return lo;
+    return lo;
     }
 };
