0fd4cdf7c8e90aebc5123b441cf736a6d7f8a9c4
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-08-01 09:26:51 +0530

    Time: 23 ms (5.05%), Space: 41 MB (8.72%) - LeetHub

---
+++ 3238-find-the-number-of-winning-players/3238-find-the-number-of-winning-players.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int winningPlayerCount(int n, vector<vector<int>>& pick) {
+        unordered_map<int, unordered_map<int, int>> freq;
+
+        for (auto &ele : pick) {
+            int player = ele[0];
+            int color = ele[1];
+            freq[player][color]++;
+        }
+        int count =0;
+        for(int i=0;i<n;i++){
+            auto det = freq[i];
+            for(auto x : det){
+                if(x.second>i){
+                    count++;
+                    break;
+                }
+            }
+        }
+    return count;
+    }
+};
