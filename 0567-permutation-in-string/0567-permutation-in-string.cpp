f3bb49fdd28a1f645b90b965c3a50a7e279f7fc4
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-09-07 00:56:30 +0530

    Time: 7 ms (40.09%), Space: 11 MB (17.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,15 +3,30 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s2.size();
         int m = s1.size();
-        sort(s1.begin(), s1.end());
 
-        for(int i=0; i<=n-m; i++){
-            string temp = s2.substr(i, m);
+        unordered_map<char, int> freq, mp;
 
-            sort(temp.begin(), temp.end());
+        for(auto ele : s1){
+            freq[ele]++;
+        }
+        
+        int i=0,j=0;
 
-            if(temp == s1) return true;
+        while(j<n) {
+            
+            mp[s2[j]]++;
 
+            if(j-i+1 > m ) {
+
+                mp[s2[i]]--;
+                
+                if (mp[s2[i]] == 0) {
+                    mp.erase(s2[i]);
+                }
+                i++;
+            }        
+            if(j-i+1 == m && freq == mp) return true;
+        j++;
         }
     return false;    
     }
