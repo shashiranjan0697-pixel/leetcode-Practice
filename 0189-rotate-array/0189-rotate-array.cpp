e97285d1d64d3219a4e92c48a6a69c0179eb899e
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-09-15 02:40:12 +0530

    Time: 0 ms (100%), Space: 263.5 MB (18.61%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+
+        int n = arr.size();
+        k = k%n;
+
+        reverse(arr.begin(), arr.end());
+    
+        reverse(arr.begin(), arr.begin()+k);
+        reverse(arr.begin()+k, arr.end());
+    }
+};
