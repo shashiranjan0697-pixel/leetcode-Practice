c5959a5f514f84bd1f0f338bdfdaf1ab7e7332b4
Shashi Ranjan <shashiranjan0697@gmail.com>
2026-09-19 11:26:02 +0530

    Time: 2 ms (13.08%), Space: 29.1 MB (6.67%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> prevPermOpt1(vector<int>& arr) {
+        int n = arr.size();
+        int breakingPoint = -1;
+        for(int i=n-2; i>=0;i--){
+            if(arr[i] > arr[i+1]){
+                breakingPoint = i;
+                break;
+            }
+        }
+
+        if(breakingPoint == -1) return arr;
+
+        int reqIdx = -1;
+        for(int i=n-1; i>breakingPoint;i--){
+            if(arr[i] < arr[breakingPoint]){
+                if(reqIdx != -1 && arr[i] >= arr[reqIdx]) {
+                    reqIdx = i;
+                }
+                else if(reqIdx == -1) {
+                    reqIdx = i;
+                }
+            }
+        }
+        cout<<breakingPoint<<", "<<reqIdx;
+        swap(arr[breakingPoint], arr[reqIdx] );
+
+    return arr;
+    }
+};
